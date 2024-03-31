f513130574faf456c0cdbc16764448c1213fd96a
chazari <chalimovrz@gmail.com>
2024-03-31 19:29:51 +0500

    fix

--- ticket_customer.go
+++ ticket_customer.go
@@ -2,7 +2,7 @@ package zammad
 
 import "fmt"
 
-func (c *Client) TicketListByCustomer(customer int) (data *[]map[string]interface{}, err error) {
+func (c *Client) TicketListByCustomer(customer int) (data *map[string]interface{}, err error) {
 	url := fmt.Sprintf("%s/api/v1/ticket_customer?customer_id=%d", c.Url, customer)
 	req, err := c.NewRequest("GET", url, nil)
 	if err != nil {
